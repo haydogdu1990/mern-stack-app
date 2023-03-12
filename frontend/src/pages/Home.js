05e4f2bc6e060f49b28d523232e814f032d25aa5
Hasan AYDOĞDU <haydogdu1990@gmail.com>
2023-03-12 13:18:30 +0300

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import axios from 'axios';
 
-import { useUsersContext } from "../hooks/useUsersContext";
+
 
 //components
 import UserDetails from "../components/UserDetails";
